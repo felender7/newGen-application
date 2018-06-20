404e2d2d8c95d51dad984e9b6d8b179daea73d30
Felender Hlungwani <felender7@gmail.com>
2018-06-20 12:36:09 +0200

    fixed some maja error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   def index
       @compose_messages_count = current_user.compose_messages.all.count
       @contacts_count = current_user.contacts.all.count
+      @compose_messages = current_user.compose_messages.all.order('Created_at DESC')
   end
 
   def welcome
