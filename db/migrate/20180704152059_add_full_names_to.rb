2b8e07fe0aafec5089b4d075f650e2134d557d82
Felender Hlungwani <felender7@gmail.com>
2018-07-05 04:19:55 +0200

    removed add first name and last name migration

--- db/migrate/20180704152059_add_full_names_to.rb
+++
@@ -1,6 +0,0 @@
-class AddFullNamesTo < ActiveRecord::Migration[5.1]
-  def change
-    add_column :contacts, :first_name, :string
-    add_column :contacts, :last_name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704152059) do
+ActiveRecord::Schema.define(version: 20180704131617) do
 
   create_table "compose_messages", force: :cascade do |t|
     t.string "recipient"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20180704152059) do
   end
 
   create_table "contacts", force: :cascade do |t|
+    t.string "firts_name"
+    t.string "last_name"
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,8 +32,6 @@ ActiveRecord::Schema.define(version: 20180704152059) do
     t.string "group"
     t.string "email"
     t.string "image"
-    t.string "first_name"
-    t.string "last_name"
     t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 20180704152059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "full_name"
-    t.string "avatar"
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
